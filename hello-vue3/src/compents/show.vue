d34016f3d1e42b1e67c8d08af6ba23ce068195d9
wanli <hyhgfrgh@163.com>
2025-11-21 22:49:45 +0800

    配置了vue router,准备写页面跳转

--- hello-vue3/src/compents/show.vue
+++ hello-vue3/src/compents/show.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="image-scroll">
-        <div v-for="item in s" :key="item.id">
+        <div v-for="item in props.s" :key="item.id">
             <img :src="item.img_url" class="scroll-img" />
             {{ item.id }}
     </div>
@@ -14,25 +14,27 @@
 
 <style scoped>
     .image-scroll {
-    display: flex;          /* 横向排列 */
-    flex-direction: row;
-    overflow-x: auto;       /* 超出部分允许横向滚动 */
-    white-space: nowrap;    /* 防止换行 */
-    padding: 10px;
-    scroll-behavior: smooth;
-    gap: 10px;              /* 图片间距 */
-    border: 1px solid #ccc; /* 可选：显示滚动区域边框 */
+        display: flex;
+        flex-direction: row;
+        overflow-x: auto;
+        white-space: nowrap;
+        padding: 10px;
+        scroll-behavior: smooth;
+        gap: 10px;
+        border: 1px solid #ccc;
     }
 
     .scroll-img {
-    height: 150px;          /* 高度固定，宽度自适应 */
-    flex-shrink: 0;         /* 防止被压缩 */
+    height: 150px;
+    flex-shrink: 0;
     border-radius: 5px;
     }
 
 </style>
 
 <script setup>
-
+    const props = defineProps({
+        s: Array
+    })
 
 </script>
