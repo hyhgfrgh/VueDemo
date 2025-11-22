8386570afa5fb13cfc7b37fb0ef5d7a151cfad64
wanli <hyhgfrgh@163.com>
2025-11-22 22:51:48 +0800

    commit

--- helloSpringBoot/src/main/java/com/wanli/hellospringboot/HelloSpringBootApplication.java
+++ helloSpringBoot/src/main/java/com/wanli/hellospringboot/HelloSpringBootApplication.java
@@ -9,8 +9,6 @@ public class HelloSpringBootApplication {
     public static void main(String[] args) {
         SpringApplication.run(HelloSpringBootApplication.class, args);
     }
-
-
-
+    
 
 }
